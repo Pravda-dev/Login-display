6a88f06483ea0ed68e2507184cc6d6c1ddbea028
Konstantin <pravda11147@gmail.com>
2023-07-22 23:54:56 +0300

    подготовка к следующему уроку

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -7,6 +7,34 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
     
+    @IBAction func signupAction(_ sender: Any) {
+        print("Signup")
+    }
+    @IBAction func loginAction(_ sender: Any) {
+        print("Login")
+    }
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    private func configureButtonLogin() {
+        // Add shadow
+        let button = loginButton
+        button?.layer.shadowColor = UIColor.systemMint.cgColor
+        button?.layer.shadowOffset = CGSize(width: -0.4, height: 2.5)
+        button?.layer.shadowOpacity = 0.7
+        button?.layer.shadowRadius = 6
+    }
+    
+    
+    //MARK: - Live cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureButtonLogin()
+        
+    }
 }
