d3e12b56e02fb6c4b106c0be003ecd0ec86293ff
Konstantin <pravda11147@gmail.com>
2023-07-23 20:33:38 +0300

    login page - логика проверки e-mail и password - изменение цвета при неправильном вводе - логика ошибки при неправильном вводе

---
+++ Login/Login/String+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  String+Ex.swift
+//  login
+//
+//  Created by pravda on 23.07.2023.
+//
+
+import Foundation
+
+extension String {
+    var localised: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
